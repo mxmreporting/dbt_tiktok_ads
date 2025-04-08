1e7fbf3844cb7a2bf64ed2321586f71612ee43cb
Sandeep Nabar <Sandeepnab@mediabymother.com>
2025-04-08 14:38:21 +0530

    Changes for Conversions

--- models/tiktok_ads__ad_group_report.sql
+++ models/tiktok_ads__ad_group_report.sql
@@ -56,7 +56,7 @@ aggregated as (
         sum(hourly.clicks) as clicks,
         sum(hourly.spend) as spend,
         sum(hourly.reach) as reach,
-        --sum(hourly.conversion) as conversions
+        sum(hourly.conversion) as conversions,
         sum(hourly.likes) as likes,
         sum(hourly.comments) as comments,
         sum(hourly.shares) as shares,
